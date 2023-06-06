c8b4f7314f0a75e72e81cd135a6c93b36eb669bc
squirogar <quirogaseb59@gmail.com>
2023-06-06 19:59:36 -0400

    arreglado la eliminacion de clusters vacios

--- Kmeans.py
+++ Kmeans.py
@@ -3,30 +3,55 @@ import sys
 
 
 class KMeans():
+    """
+    Clase que ejecuta el algoritmo de clustering K-means
+
+    Args:
+        - num_clusters (int): número de clusters o grupos en los que se va a agrupar la data.
+        - num_ejecuciones (int): número de ejecuciones (runs) del algoritmo K-means.
+        - num_iter (int): número de iteraciones por cada ejecución del algoritmo.
+
+    """
 
     def __init__(self, num_clusters, num_ejecuciones=1, num_iter=10):
         self.__K = num_clusters
         self.__num_runs = num_ejecuciones
         self.__num_iterations = num_iter
-        
-
 
     
     def fit(self, data, verbose=False):
+        """
+        Ajusta el algoritmo de clustering K-means a la data.
+
+        Args:
+            - data (ndarray (m,n)): dataset
+            - verbose (bool): imprime por pantalla información más detallada
+
+        Returns:
+            - hist_cost_best_run (list): lista con el historial de costo de la mejor ejecución hecha
+            - centroids_best_run (ndarray(K,n)): numpy array con los centroids de cada cluster de la 
+                                                 mejor ejecución hecha
+            - index_centroids_best_run (ndarray (m,)): numpy array con los índices de los centroids 
+                                                       asignados a cada ejemplo de data de la mejor
+                                                       ejecución hecha
+        """
         min_cost = sys.float_info.max
         hist_cost_best_run = None
         centroids_best_run = None
         index_centroids_best_run = None
 
         for i in range(self.__num_runs):
+            print(f"\nComienza la ejecucion {i}:")
             initial_centroids = self.__init_centroids(data, self.__K)
             historial_cost, centroids, index_centroids = self.__run_Kmeans(data, initial_centroids, self.__num_iterations, verbose)
             # seleccionar el último costo de cada ejecución y comparar y obtener el menor costo
             if historial_cost[-1] < min_cost:
+                min_cost = historial_cost[-1]
                 hist_cost_best_run = historial_cost
                 centroids_best_run = centroids
                 index_centroids_best_run = index_centroids 
 
+            print(f"menor costo encontrado hasta el momento: {hist_cost_best_run[-1]}") 
         
         # retornar el costo, centroids y los index_centroids de la ejecución con el menor costo
         return hist_cost_best_run, centroids_best_run, index_centroids_best_run
@@ -69,9 +94,9 @@ class KMeans():
         Returns:
             - cost (list): lista con los costos calculados en cada iteración.
             - centroids (ndarray (K,n)): numpy array con los centroids de cada cluster
-                                       encontrados por K-means.
-            - index_centroids (ndarray (m,)): numpy array con los índices de los centroids
-                                            asignados a cada ejemplo X[i].
+                                         encontrados por K-means.
+            - index_centroids (ndarray (m,)): numpy array con los índices de los centroids 
+                                              asignados a cada ejemplo X[i]
         """
         cost = []
         centroids = initial_centroids
@@ -80,10 +105,18 @@ class KMeans():
         
         print("Running K-means algorithm ...")
         for i in range(num_iterations):
+            print(f"Iteration {i}")
+            # asignamos los ejemplos al cluster más cercano
             index_centroids = self.__find_closests_centroids(X, centroids)
+            # movemos los centroids al promedio de los ejemplos del cluster
             centroids = self.__move_centroids(X, K, index_centroids)
             
+            # funcion de costo
             cost.append(self.compute_cost(X, centroids, index_centroids))
+            
+            # eliminamos los clusters vacíos
+            centroids = self.__delete_empty_clusters(index_centroids, centroids)
+            K = centroids.shape[0]
             
             if verbose:
                 print(f"Costo en iteracion {i}: {cost[i]}") # costo
@@ -100,20 +133,21 @@ class KMeans():
         
         Args:
             - X (ndarray (m,n)): dataset
-            - centroids (ndarray (K,n)): K centroids
+            - centroids (ndarray (K,n)): numpy array con los K centroids.
             
         Returns:
-            - index_cent (ndarray (m,)): numpy array con los índices de los centroids más cercanos para cada ejemplo. 
+            - index_cent (ndarray (m,)): numpy array con los índices de los centroids asignados 
+                                         a cada ejemplo X[i]
         """
         
         index_cent = np.zeros(X.shape[0], dtype=int) # vector de indices
         
         for i, ejemplo in enumerate(X):
-            distancias = np.zeros(centroids.shape[0]) # K distancias, una para centroid
+            distancias = [] #np.zeros(centroids.shape[0], dtype=float) # K distancias, una para centroid
             
             for j, cent in enumerate(centroids):
                 dist = np.linalg.norm(ejemplo - cent) ** 2 # distancia entre el ejemplo i y el centroid j
-                distancias[j] = dist
+                distancias.append(dist)
             
             index_cent[i] = np.argmin(distancias) # se le asigna al ejemplo i el centroid más cercano
         
@@ -127,22 +161,60 @@ class KMeans():
         Args:
             - X (ndarray (m,n)): dataset
             - K (int): número de centroids
-            - index_cent (ndarray (m,)): centroids asignados para cada ejemplo dentro del dataset X.
+            - index_cent (ndarray (m,)): numpy array con los índices de los centroids 
+                                         asignados a cada ejemplo X[i]
 
         Returns:
             - centroids (ndarray (K, n)): K centroids con sus nuevas ubicaciones
         
         """
         
+        
         centroids = np.zeros((K, X.shape[1]))
         for k in range(K):
             ejemplos_cluster = X[index_cent == k] # ejemplos asignados al centroid k
-            centroids[k] = np.mean(ejemplos_cluster, axis=0) # media de los ejemplos del cluster k
+            if ejemplos_cluster.size > 0: # si hay ejemplos dentro del cluster k
+                centroids[k] = np.mean(ejemplos_cluster, axis=0) # media de los ejemplos del cluster k
         
         return centroids
 
+    
+    
+    def __delete_empty_clusters(self, index_centroids, centroids):
+        """
+        Elimina los centroids del array de centroids cuyos clusters están vaciós. 
 
+        Args: 
+            - index_centroids (ndarray (m,)): numpy array con los índices de los centroids 
+                                              asignados a cada ejemplo de data
+            - centroids (ndarray (K,n)): numpy array con los K centroids de cada cluster
 
+        Returns:
+            - centroids (ndarray (*,n)): numpy array de centroids cuyos clusters no están vacíos.
+                                         Con 1 <= * <= K.
+        """
+        K = centroids.shape[0]
+        
+        clusters_a_eliminar = []
+        for k in range(K):
+            if k not in index_centroids: # si el cluster k está vacío
+                print(f"el cluster {k} esta vacio, será eliminado.")
+                clusters_a_eliminar.append(k)
+
+        if len(clusters_a_eliminar):
+            print(f"antes: {centroids.shape}")
+            centroids = np.delete(centroids, clusters_a_eliminar, axis=0)
+            print(f"despues: {centroids.shape}")
+
+        # borrar
+        #unique, counts = np.unique(centroids, return_counts=True)
+        #print(f"coincidencias2: {dict(zip(unique, counts))}") 
+        
+        return centroids
+    
+    
+    
+    
     def compute_cost(self, X, centroids, index_cent):
         """
         Retorna el costo calculado a partir de la función de costo o distortion que el algoritmo 
@@ -153,7 +225,8 @@ class KMeans():
         Args:
             - X (ndarray (m,n)): data
             - centroids (ndarray (K, n)): centroids de cada cluster
-            - index_cent (ndarray (m,)): índice del centroid asignado a cada ejemplo X[i]
+            - index_cent (ndarray (m,)): numpy array con los índices de los centroids 
+                                         asignados a cada ejemplo X[i].
         
         Returns:
             - cost (float): costo o distancia promedio entre los ejemplos y sus centroids.
@@ -163,7 +236,8 @@ class KMeans():
         cost = 0.0
         for k, cent in enumerate(centroids):
             ejemplos = X[index_cent == k] # todos los ejemplos en el cluster k
-            cost = cost + np.linalg.norm(ejemplos - cent) ** 2 # costo del cluster k
+            if ejemplos.size > 0:
+                cost = cost + np.linalg.norm(ejemplos - cent) ** 2 # costo del cluster k
         
         cost = cost / m
         return cost
